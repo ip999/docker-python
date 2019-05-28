cce6ae212376fc14cfe9b6cbd115a04a74dc7a06
ip999 <porteous@gmail.com>
2019-05-28 14:28:18 +0100

    Update ip.py

--- ip.py
+++ ip.py
@@ -1,4 +1,11 @@
 import requests
+import json
 url = "https://ifconfig.co/json"
 content = requests.get(url)
-print(content.text)
+
+# print out entire JSON response
+print("JSON Body:\n" + content.text)
+
+#print out just the IP address
+process_content = json.loads(content.text)
+print("IP: " + process_content["ip"])
